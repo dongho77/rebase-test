8275f4b36005337ffaf273f37ace472642fa234d
id_dongho <dho7@onycom.com>
2024-01-05 19:09:48 +0900

    husky test

--- scripts/validationHotfix.js
+++ scripts/validationHotfix.js
@@ -1,7 +1,7 @@
 import { execSync } from "child_process"
 
 function getCurrentBranch() {
-    return execSync("git rev-parse --abbrev-ref HEAD").toString();
+    return execSync("git rev-parse --abbrev-ref HEAD");
 }
 
 console.log("Current branch: " + getCurrentBranch());

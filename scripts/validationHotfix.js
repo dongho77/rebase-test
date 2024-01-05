b77c0a072ca52f822b6dd4e61944772b0354c0cf
id_dongho <dho7@onycom.com>
2024-01-05 19:14:20 +0900

    husky test

--- scripts/validationHotfix.js
+++ scripts/validationHotfix.js
@@ -4,4 +4,10 @@ function getCurrentBranch() {
     return execSync("git rev-parse --abbrev-ref HEAD").toString().trim();
 }
 
-console.log("Current branch: " + getCurrentBranch());
+const currentBranch = getCurrentBranch();
+const invalidPatterns = [/dev/, /develop/]; // 필터링할 패턴 목록
+
+if (currentBranch.startsWith('hotfix/') && invalidPatterns.some(pattern => pattern.test(currentBranch))) {
+    console.error('Error: Hotfix branch name should not contain "dev" or "develop".');
+    process.exit(1);
+}

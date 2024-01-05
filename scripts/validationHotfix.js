26b397362739505e28927b86e8292bc4e7ac4949
id_dongho <dho7@onycom.com>
2024-01-05 19:21:22 +0900

    hotfix branch validation add

--- scripts/validationHotfix.js
+++ scripts/validationHotfix.js
@@ -8,6 +8,6 @@ const currentBranch = getCurrentBranch();
 const invalidPatterns = [/dev/, /develop/]; // 필터링할 패턴 목록
 
 if (currentBranch.startsWith('hotfix/') && invalidPatterns.some(pattern => pattern.test(currentBranch))) {
-    console.error('Error: Hotfix branch name should not contain "dev" or "develop".');
+    console.log('Error: Hotfix branch name should not contain "dev" or "develop".');
     process.exit(1);
 }
